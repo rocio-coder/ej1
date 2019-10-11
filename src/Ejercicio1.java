ddb2f68784df0b219870c06da42fc507f663e02d
rocio-corder <suarez.rocio96@gmail.com>
2019-10-11 20:45:41 -0300

    Initial commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        double primernumero;
+        double segundonumero;
+        double resultado;
+        primernumero = 7.8;
+        segundonumero=8.9;
+        System.out.println("el resultado de "+ primernumero + segundonumero);
+    }
+}
